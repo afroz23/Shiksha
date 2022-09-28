b729889e7e5abc0139f9909f71322415c32a1080
afroz23 <afroziiitian@gmail.com>
2022-09-28 11:39:48 +0530

    package error fixed

--- app.js
+++ app.js
@@ -43,10 +43,10 @@ require("./src/routes/auth.js")(auth);
 app.use("/auth", auth);
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./client/build")));
+  app.use(express.static(path.join(__dirname, "./frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "./client/build/index.html"))
+    res.sendFile(path.resolve(__dirname, "./frontend/build/index.html"))
   );
 } else {
   app.get("/", (req, res) => {
