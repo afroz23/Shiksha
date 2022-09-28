be1ec5d4142c466cc5ea5b65abe82c83996fd938
afroz23 <afroziiitian@gmail.com>
2022-09-28 11:58:58 +0530

    server modified

--- app.js
+++ app.js
@@ -43,11 +43,12 @@ require("./src/routes/auth.js")(auth);
 app.use("/auth", auth);
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "./frontend/build")));
+  app.use(express.static(path.join(__dirname, "frontend", "build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "./frontend/build/index.html"))
-  );
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  });
+
 } else {
   app.get("/", (req, res) => {
     res.send("API is running..");
@@ -58,3 +59,6 @@ if (process.env.NODE_ENV === "production") {
 app.listen(port,()=>{
   console.log(`server listening on PORT ${port}`);
 })
+
+
+
