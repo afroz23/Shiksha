066735ef82461fdb8a997adebbb8f5327bb05629
afroz23 <afroziiitian@gmail.com>
2022-09-28 13:10:52 +0530

    Reverted

--- app.js
+++ app.js
@@ -43,12 +43,20 @@ require("./src/routes/auth.js")(auth);
 app.use("/auth", auth);
 const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
+<<<<<<< HEAD
   app.use(express.static(path.join(__dirname, "frontend", "build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
   });
 
+=======
+  app.use(express.static(path.join(__dirname, "./client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "./client/build/index.html"))
+  );
+>>>>>>> 846c3fc (Revert "package error fixed")
 } else {
   app.get("/", (req, res) => {
     res.send("API is running..");
